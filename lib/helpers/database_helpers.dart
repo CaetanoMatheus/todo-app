0da013effc4e28328d537c8d2c1b4cfc060375e9
Matheus Caetano <caetanomatheus876@gmail.com>
2020-12-10 10:04:46 -0300

    Database Helper

---
+++ lib/helpers/database_helpers.dart
@@ -0,0 +1,43 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  Database _database;
+
+  final String _databaseName = 'todo_app.db';
+  final int _databaseVersion = 1;
+
+  static DatabaseHelper _instance;
+
+  DatabaseHelper._();
+
+  static DatabaseHelper getInstance() {
+    if (_instance == null) _instance = new DatabaseHelper._();
+    return _instance;
+  }
+
+  Future<String> _getDatabasePath() async {
+    String databasesPath = await getDatabasesPath();
+    return join(databasesPath, this._databaseName);
+  }
+
+  Future<Database> _initDatabase() async {
+    String databasePath = await this._getDatabasePath();
+    return await openDatabase(
+      databasePath,
+      version: this._databaseVersion,
+      onCreate: this._onDatabaseCreate,
+    );
+  }
+
+  void _onDatabaseCreate(Database database, int newerVersion) async {}
+
+  Future destroyDabase() async {
+    await deleteDatabase(await this._getDatabasePath());
+  }
+
+  Future<Database> get database async {
+    if (this._database == null) this._database = await this._initDatabase();
+    return this._database;
+  }
+}
