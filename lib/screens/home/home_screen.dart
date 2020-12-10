765bac99558d9da2fcf42051d276a480a3e0c9c0
Matheus Caetano <caetanomatheus876@gmail.com>
2020-12-09 21:15:26 -0300

    App bar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,6 +3,25 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.purple);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: buildAppBar(),
+      drawer: Drawer(),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      iconTheme: IconThemeData(color: Colors.grey[600]),
+      actions: [
+        IconButton(icon: Icon(Icons.search), onPressed: null),
+        IconButton(icon: Icon(Icons.notifications_outlined), onPressed: null),
+      ],
+    );
   }
 }
